f9d751611de202fbb808c61c1718039a793c345e
williamstyronejr <itenory@gmail.com>
2021-04-08 14:01:27 -0400

    Moved server start to its own file

--- server/src/index.js
+++ server/src/index.js
@@ -1,42 +1,4 @@
 require('dotenv').config();
-const http = require('http');
-const path = require('path');
-const jobs = require('./services/jobs');
-const { connectDatabase, disconnectDatabase } = require('./services/database');
-const { setupRedis } = require('./services/redis');
-const { verifyGcloudConfig } = require('./utils/utils');
-const app = require('./services/app');
-const logger = require('./services/winston');
+const startServer = require('./server');
 
-const {
-  IP,
-  PORT,
-  DB_URI,
-  REDIS_HOST,
-  REDIS_PORT,
-  REDIS_URL,
-  GCLOUD_APPLICATION_CREDENTIALS,
-} = process.env;
-
-verifyGcloudConfig(path.join(__dirname, '../', GCLOUD_APPLICATION_CREDENTIALS))
-  .then(() => {
-    // Connect to Database
-    connectDatabase(DB_URI).then(() => {
-      setupRedis(REDIS_PORT, REDIS_HOST, REDIS_URL).then(() => {
-        // Set up scheduler
-        jobs.setUpJobs();
-
-        // Create http server
-        const server = http.createServer(app);
-        server.listen(PORT, IP, () => {
-          console.log(`Server running at ${IP}:${PORT}`);
-        });
-      });
-    });
-  })
-  .catch((err) => {
-    // Log server breaking error, even on production
-    disconnectDatabase();
-    logger.debug(err.message);
-    process.exit(1);
-  });
+startServer();
