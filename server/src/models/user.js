416e6c1fd04a698d1a062c76a5a3028d04c9e1ec
williamstyronejr <itenory@gmail.com>
2021-04-13 06:36:02 -0400

    Add a display name field

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  displayName: { type: String, default: '' },
   email: { type: String, required: true, unique: true },
   hash: { type: String, required: true, unique: true },
   followers: { type: Number, default: 0 },
