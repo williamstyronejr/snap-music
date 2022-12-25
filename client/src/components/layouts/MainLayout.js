6d8c1a04be9c61ecc4972b9542751cfb7763d382
williamstyronejr <itenory@gmail.com>
2022-12-25 01:10:32 -0500

    Updated for linting

--- client/src/components/layouts/MainLayout.js
+++ client/src/components/layouts/MainLayout.js
@@ -7,24 +7,22 @@ import Header from './Header';
 
 const MainLayout = (props) => {
   return (
-    <>
-      <div
-        className={`layout layout-main ${
-          props.user.nightMode ? 'layout-night' : ''
-        } ${
-          props.mediaPlayer.footer && props.mediaPlayer.playlist.length > 0
-            ? 'layout--footer'
-            : ''
-        }`}
-      >
-        <Header user={props.user} />
-        <NotificationContainer />
-        <main className="page-main" role="main">
-          {props.children}
-        </main>
-        <MediaPlayer />
-      </div>
-    </>
+    <div
+      className={`layout layout-main ${
+        props.user.nightMode ? 'layout-night' : ''
+      } ${
+        props.mediaPlayer.footer && props.mediaPlayer.playlist.length > 0
+          ? 'layout--footer'
+          : ''
+      }`}
+    >
+      <Header user={props.user} />
+      <NotificationContainer />
+      <main className="page-main" role="main">
+        {props.children}
+      </main>
+      <MediaPlayer />
+    </div>
   );
 };
 
