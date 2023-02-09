988cb89a5218ffc5964c9a3a1dc7e67965da348f
williamstyronejr <itenory@gmail.com>
2023-02-08 20:09:27 -0500

    Now grabs genre data from server

--- client/src/components/discovery/DiscoveryPage.js
+++ client/src/components/discovery/DiscoveryPage.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { genreList } from '../../utils/utils';
+import useGenresData from '../shared/useGenresData';
+import Loading from '../shared/Loading';
 import './styles/discoveryPage.css';
 
 const DiscoveryPage = () => {
+  const { loading, genres } = useGenresData();
+
   return (
     <section className="discovery">
       <h1 className="discovery__header">Discovery by Genre</h1>
       <div className="discovery__grid">
-        {genreList.map((genre) => (
-          <div className="grid__item" key={`genre-${genre}`}>
-            <Link to={`/discovery/${genre}`} className="grid__link">
-              <span className="genre">{genre}</span>
+        {loading ? <Loading /> : null}
+
+        {genres.map((genre) => (
+          <div className="grid__item" key={`genre-${genre.id}`}>
+            <Link to={`/discovery/${genre.name}`} className="grid__link">
+              <span className="genre">{genre.name}</span>
+              {genre.image ? (
+                <img
+                  className="discovery__genre-img"
+                  src={`/img/${genre.image}`}
+                  alt=""
+                />
+              ) : null}
             </Link>
           </div>
         ))}
