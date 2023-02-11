b188de10c539559f22f9fac9d54d9629adaf6c91
williamstyronejr <itenory@gmail.com>
2023-02-11 16:05:00 -0500

    Cron job handling for serverless deployment

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const trackRouter = require('./track');
 const userRouter = require('./user');
+const cronRouter = require('./cron');
+
+const { SERVERLESS } = process.env;
 
 module.exports = function router(app) {
   // Setting up app routes
   app.use(userRouter);
   app.use(trackRouter);
+  if (SERVERLESS === 'true') app.use(cronRouter);
 
   // Default to react app
   app.use('/*', (req, res) => {
